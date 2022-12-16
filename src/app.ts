387380faf47322670ceeab3400c2fb8a01748c9c
purrsong-koohoyn <ninestring@purrsong.com>
2022-12-16 15:35:38 +0900

    feat: Add/database connection

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,33 @@
-import express, { Request, Response, NextFunction } from "express";
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import { db } from "./database";
 
 const app = express();
 
-app.get("/welcome", (req: Request, res: Response, next: NextFunction) => {
+app.get("/welcome", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await db.query("INSERT INTO successLog(name) VALUES('호호호호')");
+  } catch (error) {
+    console.log(":::error:::welcome:::", error);
+  }
+
   res.send("welcome!");
 });
+
+app.get(
+  "/healthcheck",
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log("healthcheck");
+
+    try {
+      await db.query("INSERT INTO successLog(name) VALUES('healthcheck')");
+    } catch (error) {
+      console.log(":::error:::healthcheck:::", error);
+    }
+
+    res.send("healthcheck!");
+  }
+);
 
 app.listen("1234", () => {
   console.log(`
